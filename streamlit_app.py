272a199050fb30e2cbc6005d9e49009fe97972c1
Shuting (Tina) Feng <57819171+TINAF1109@users.noreply.github.com>
2022-02-26 11:10:35 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -109,7 +109,7 @@ if st.checkbox("Show Raw Data"):
 ## Plot 1: TOP billionaires by Year
 st.subheader("1. Show yearly top 10 billionaires by year selected")
 year = st.selectbox("Year", df['year'].unique())
-st.write(df[df['year']==year][['name','wealth_worth_in_billions','rank']].sort_values('wealth_worth_in_billions', ascending = False).head(10).set_index('rank'))
+#st.write(df[df['year']==year][['name','wealth_worth_in_billions','rank']].sort_values('wealth_worth_in_billions', ascending = False).head(10).set_index('rank'))
 new_df=df[df['year']==year][['name','wealth_worth_in_billions','rank']].sort_values('wealth_worth_in_billions', ascending = False).head(10).set_index('rank')
 st.dataframe(data=new_df, width=700, height=768)
 
