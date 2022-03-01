4e2dbb0693a060c47fe098a051796c9a0ea6d4da
Shuting (Tina) Feng <57819171+TINAF1109@users.noreply.github.com>
2022-03-01 13:31:00 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -120,7 +120,8 @@ st.write(' ')
 
 ## Plot 1: TOP Billionaires by Year
 st.header("1. Show TOP 10 billionaires by Year Selected")
-st.subheader("Who are the TOP 10 billionaires and what's their wealth in 1996, 2001, and 2014?")
+st.subheader("👉Who are the TOP 10 billionaires and what's their wealth in 1996, 2001, and 2014?")
+st.caption("Who are the TOP 10 billionaires and what's their wealth in 1996, 2001, and 2014?")
 st.markdown('Select year from the dropdown menu to see corresponding TOP 10 billionaires in dataframe and barchart!') 
 
 
